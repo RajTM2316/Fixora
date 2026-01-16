594d02c53350738a987a3d579b3d1b15e1baac3a
Raj Mevawala <rajmevawala23@icloud.com>
2026-01-16 11:13:50 +0530

    [IMP] Connect Postgre SQL as External Database

--- fixora_server/mysite/settings.py
+++ fixora_server/mysite/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fixora',
+        'USER': 'postgres',
+        'PASSWORD': '2316',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
