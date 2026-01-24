f9161d7dec47e3097fe8ea9dcb91dbb0bf80d9e8
Bhaumik-05 <modibhaumik180@gmail.com>
2026-01-24 14:11:22 +0530

    [IMP] create app manage_user and models

--- fixora_server/manage_user/models.py
+++ fixora_server/manage_user/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Address(models.Model):
+    street = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    pincode = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.city} - {self.pincode}"
+
+
+class Profile(models.Model):
+    ROLE_CHOICES = [
+        ('customer', 'Customer'),
+        ('provider', 'Service Provider'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    phone = models.CharField(max_length=15)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
