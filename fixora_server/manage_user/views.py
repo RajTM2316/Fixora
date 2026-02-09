0789f0c759bef221aa323eb847d7074067e5b937
Bhaumik-05 <modibhaumik180@gmail.com>
2026-02-09 12:44:20 +0530

    [FIX] Fix template and login page

--- fixora_server/manage_user/views.py
+++ fixora_server/manage_user/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+
 def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+
+            # redirect based on role
+            if hasattr(user, "profile") and user.profile.role == "provider":
+                return redirect("/provider-dashboard/")
+            else:
+                return redirect("/")  # customer home
+        else:
+            messages.error(request, "Invalid username or password")
+
     return render(request, "manage_user/login.html")
