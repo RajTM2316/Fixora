73ba9462bcb2228185bb9bd45b75606ccf792209
Raj Mevawala <rajmevawala23@icloud.com>
2026-02-15 11:14:35 +0530

    [IMP] Add latitude ,longitude in Profile Model

--- fixora_server/manage_user/models.py
+++ fixora_server/manage_user/models.py
@@ -40,5 +40,9 @@ class Profile(models.Model):
         blank=True
     )
 
+    #For Live-Location Tracking
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+
     def __str__(self):
         return self.user.username
