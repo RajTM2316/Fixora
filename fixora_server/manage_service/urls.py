cd3f8964f93da5eb683058d25740a6b22dfe6c5e
Raj Mevawala <rajmevawala23@icloud.com>
2026-02-15 12:56:11 +0530

    [FEAT] Live Location Sharing

--- fixora_server/manage_service/urls.py
+++ fixora_server/manage_service/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 from manage_user.views import save_location
+from manage_user.models import Profile
+
 urlpatterns = [
     path("customer/home/", views.customer_home, name="customer_home"),
     path("provider/dashboard/", views.provider_dashboard, name="provider_dashboard"),
     path("add_category/", views.add_category, name="add_category"),
-    path('api/save-location/', save_location, name='save_location')
+    path('api/save-location/', save_location, name='save_location'),
+    path('location-map/', views.location_map, name='location_map'),
 ]
--- fixora_server/manage_service/views.py
+++ fixora_server/manage_service/views.py
@@ -3,11 +3,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Category
 from django.contrib import messages
-
-@login_required
-def customer_home(request):
-    return render(request, "manage_service/customer_home.html")
-
+from manage_user.views import save_location
+from manage_user.models import Profile
 @login_required
 def provider_dashboard(request):
     return render(request, "manage_service/provider_dashboard.html")
@@ -31,3 +28,11 @@ def add_category(request):
             messages.success(request, "Category added successfully.")
             return redirect("add_category")
     return render(request, "manage_service/add_category.html")
+
+def location_map(request):
+    profile = Profile.objects.get(user=request.user)
+
+    return render(request, "manage_service/location_map.html", {
+        "latitude": profile.latitude,
+        "longitude": profile.longitude
+    })
