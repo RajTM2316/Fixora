573612767e16b744002a9268c92e388476f5c61a
Raj Mevawala <rajmevawala23@icloud.com>
2026-02-24 19:23:58 +0530

    [FEAT] custom AdminSite with isolated logout redirection

--- fixora_server/manage_user/admin.py
+++ fixora_server/manage_user/admin.py
@@ -1,5 +1,22 @@
-from django.contrib import admin
+from django.contrib.admin import AdminSite
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 from .models import Profile, Address
 
-admin.site.register(Address)
-admin.site.register(Profile)
+
+class FixoraAdminSite(AdminSite):
+    site_header = "Fixora Control Panel"
+    site_title = "Fixora Admin"
+    index_title = "Welcome to Fixora Admin"
+
+    def logout(self, request, extra_context=None):
+        logout(request)
+        return redirect('/admin/login/')
+
+
+# Create custom admin instance
+custom_admin_site = FixoraAdminSite(name='fixora_admin')
+
+# Register models here
+custom_admin_site.register(Profile)
+custom_admin_site.register(Address)
--- fixora_server/mysite/urls.py
+++ fixora_server/mysite/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path ,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from manage_user.admin import custom_admin_site
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', custom_admin_site.urls),
     path('payments/', include('payments.urls')),
     path("", include('manage_user.urls')),
     path("manage_service/", include('manage_service.urls')),
