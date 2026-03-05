1ffa39bb26b6a4a8d16ae9720ec3f191a9a742bd
Manavmodi0807 <manavmodi.0807@gmail.com>
2026-03-05 19:44:06 +0530

    [FEAT]create payment model for razorpay integration

--- fixora_server/payments/models.py
+++ fixora_server/payments/models.py
@@ -1,14 +1,31 @@
 from django.db import models
 
-# Create your models here.
-
 class Payment(models.Model):
+
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    timestamp = models.DateTimeField(auto_now_add=True)
+
     description = models.CharField(max_length=255)
 
+    razorpay_order_id = models.CharField(max_length=255, null=True, blank=True)
+
+    razorpay_payment_id = models.CharField(max_length=255, blank=True, null=True)
+
+    razorpay_signature = models.CharField(max_length=500, blank=True, null=True)
+
+    status = models.CharField(
+    max_length=50,
+    choices=[
+        ("created", "Created"),
+        ("paid", "Paid"),
+        ("failed", "Failed"),
+    ],
+    default="created"
+)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
-        return f"Payment of {self.amount} on {self.timestamp}"
+        return f"Payment {self.razorpay_order_id} - Amount {self.amount}"
 
 class Invoice(models.Model):
     payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
