a6076420d61f0e539f701f213fabc4088e27ba06
Manavmodi0807 <manavmodi.0807@gmail.com>
2026-03-05 20:59:46 +0530

    [add] razorpay checkout payment templates

--- fixora_server/payments/urls.py
+++ fixora_server/payments/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import create_payment, payment_success
 
 urlpatterns = [
-
     path("pay/", create_payment, name="create_payment"),
 
     path("success/", payment_success, name="payment_success"),
