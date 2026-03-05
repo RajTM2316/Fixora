abae61a20fd489baf992f26dc7805c29cfd753b6
Manavmodi0807 <manavmodi.0807@gmail.com>
2026-03-05 20:37:01 +0530

    [add] create razorpay order creation view

--- fixora_server/payments/views.py
+++ fixora_server/payments/views.py
@@ -1,4 +1,26 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.conf import settings
+from .models import Payment
+from .utils import client
 
-def payment_placeholder(request):
-    return HttpResponse("Hello, this is the payments app placeholder view.")
+
+def create_payment(request):
+
+    amount = 50000
+
+    order = client.order.create({
+        "amount": amount,
+        "currency": "INR",
+        "payment_capture": 1
+    })
+
+    payment = Payment.objects.create(
+        amount=amount/100,
+        description="Fixora Service Payment",
+        razorpay_order_id=order["id"]
+    )
+
+    return render(request, "payments/payment.html", {
+        "order": order,
+        "key": settings.RAZORPAY_KEY_ID
+    })
