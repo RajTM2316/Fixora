6aa73489a62cf121ff665d8f80ca3f7c209ceeb8
Manavmodi0807 <manavmodi.0807@gmail.com>
2026-03-05 20:40:47 +0530

    [FEAT]add razorpay payment verification logic

--- fixora_server/payments/views.py
+++ fixora_server/payments/views.py
@@ -24,3 +24,33 @@ def create_payment(request):
         "order": order,
         "key": settings.RAZORPAY_KEY_ID
     })
+
+def payment_success(request):
+
+    payment_id = request.POST.get("razorpay_payment_id")
+    order_id = request.POST.get("razorpay_order_id")
+    signature = request.POST.get("razorpay_signature")
+
+    params_dict = {
+        "razorpay_payment_id": payment_id,
+        "razorpay_order_id": order_id,
+        "razorpay_signature": signature
+    }
+
+    try:
+
+        client.utility.verify_payment_signature(params_dict)
+
+        payment = Payment.objects.get(razorpay_order_id=order_id)
+
+        payment.razorpay_payment_id = payment_id
+        payment.razorpay_signature = signature
+        payment.status = "paid"
+
+        payment.save()
+
+        return render(request, "payments/success.html")
+
+    except:
+
+        return render(request, "payments/failure.html")
